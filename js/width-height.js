c111fbae200574fc70db5fea3ae535367be5fedb
Steven Wu <master_msp1@hotmail.com>
2019-05-21 01:22:27 +0200

    huh

--- js/width-height.js
+++ js/width-height.js
@@ -1,10 +1,7 @@
 $(function() {
     //This function makes it possible to square images by getting the width of the given image and replace its height with width to make a square
-    var imageSquareResizer = (selector) => {
-        selector.height(selector.width());
-    }
-    if($('.corevalue .card-deck .card-header ').length) {
-        console.log('test');
-        imageSquareResizer($('.corevalue .card-deck .card-header'));
-    }
+    var imageSquareResizer = selector => { selector.height(selector.width()) };
+
+    if($('.corevalue .card-deck .card-header').length) imageSquareResizer($('.corevalue .card-deck .card-header'));
+    if($('.skills .pro-skills .skill-holder').length) imageSquareResizer($('.skills .pro-skills .skill-holder'));
 });
