0957ea4674074cf4d5995efbf24f0aeb2cc04c4e
Steven Wu <master_msp1@hotmail.com>
2019-05-22 01:42:17 +0200

    compressed src files, added scroll close menu

--- js/navigation.js
+++ js/navigation.js
@@ -3,27 +3,36 @@ window.onload = () => {
     let icn = document.querySelector('span.fas');
     let nav = document.querySelector('header');
     let anchors = document.querySelectorAll('.nav-link');
+    let close = () => {
+        nav.classList.remove('open');
+        menu.classList.remove('open');
+        icn.classList.remove('fa-times');
+        icn.classList.add('fa-bars');
+    };
+    let open = () => {
+        nav.classList.add('open');
+        menu.classList.add('open');
+        icn.classList.remove('fa-bars');
+        icn.classList.add('fa-times');
+    }
     
     menu.onclick = () => {
         if (nav.classList.contains('open')) {
-            nav.classList.remove('open');
-            menu.classList.remove('open');
-            icn.classList.remove('fa-times');
-            icn.classList.add('fa-bars');
+            close();
         } else {
-            nav.classList.add('open');
-            menu.classList.add('open');
-            icn.classList.remove('fa-bars');
-            icn.classList.add('fa-times');
+            open();
         }
     };
 
     anchors.forEach(anchor => {
         anchor.onclick = () => {
-            nav.classList.remove('open');
-            menu.classList.remove('open');
-            icn.classList.remove('fa-times');
-            icn.classList.add('fa-bars');
+            close();
         }
     });
+
+    window.onscroll = () => {
+        if(nav.classList.contains('open')) {
+            close();
+        }
+    }
 };
